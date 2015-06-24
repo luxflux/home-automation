d611a8311164e189cfe64089efc5a237af41847a
Raffael Schmid <raffael@yux.ch>
2015-06-24 22:47:25 +0200

    add temperature and humidity

--- experiment1/experiment1.ino
+++ experiment1/experiment1.ino
@@ -1,3 +1,21 @@
+// 
+
+#include "DHT.h"
+//#include <Adafruit_CC3000.h>
+//#include <SPI.h>
+//#include <CC3000_MDNS.h>
+
+//#define ADAFRUIT_CC3000_IRQ   4
+//#define ADAFRUIT_CC3000_VBAT  5
+//#define ADAFRUIT_CC3000_CS    10
+
+#define DHTPIN 3 
+#define DHTTYPE DHT22
+
+//#define WLAN_SSID "yuxHome"
+//#define WLAN_PASS "schmidswlan"
+//#define WLAN_SECURITY WLAN_SEC_WPA2
+
 int photocellPin = 0;
 int photocellReading;
 int photocellReadingWhenPoweringOn = 0;
@@ -9,25 +27,34 @@ int movement = 0;
 int relayPin = 13;
 
 int thresholdOnOffPin = 1;
-int thresholdOnOff = 0;
+int thresholdOnOff;
 
 int thresholdMaxPin = 2;
-int thresholdMax = 720;
+int thresholdMax;
 
 int uvPin = 4;
 int uvReading;
 
 boolean on = false;
 
+DHT dht(DHTPIN, DHTTYPE);
+int temperatureReading;
+int humidityReading;
+
+//Adafruit_CC3000 cc3000 = Adafruit_CC3000(ADAFRUIT_CC3000_CS, 
+//ADAFRUIT_CC3000_IRQ, ADAFRUIT_CC3000_VBAT, SPI_CLOCK_DIV2);
+
+//MDNSResponder mdns;
+
 // the setup function runs once when you press reset or power the board
 void setup() {
   pinMode(relayPin, OUTPUT);
-  pinMode(movementPin, INPUT);
-  pinMode(thresholdOnOffPin, INPUT);
-  pinMode(thresholdMaxPin, INPUT);
+  //pinMode(movementPin, INPUT);
+  //pinMode(thresholdOnOffPin, INPUT);
+  //pinMode(thresholdMaxPin, INPUT);
   pinMode(uvPin, INPUT);
   
-  //digitalWrite(relayPin, LOW);
+  digitalWrite(relayPin, LOW);
   
   Serial.begin(9600);
 }
@@ -36,8 +63,10 @@ void loop() {
   photocellReading = analogRead(photocellPin);
   movement = digitalRead(movementPin);
   thresholdOnOff = analogRead(thresholdOnOffPin);
+  thresholdMax = analogRead(thresholdMaxPin);
   uvReading = analogRead(uvPin);
-  //thresholdMax = analogRead(thresholdMax);
+  temperatureReading = (uint8_t)dht.readTemperature();
+  humidityReading = (uint8_t)dht.readHumidity();
  
   Serial.println("============================================");
   Serial.print("Analog reading = ");
@@ -54,6 +83,10 @@ void loop() {
   Serial.println(thresholdMax);
   Serial.print("uvReading = ");
   Serial.println(uvReading);
+  Serial.print("temperatureReading = ");
+  Serial.println(temperatureReading);
+  Serial.print("humidityReading = ");
+  Serial.println(humidityReading);
   Serial.print("Lamp is on: ");
   Serial.println(on);
   
