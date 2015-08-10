3032c60451d542d475b5440a73cb8e9681c492bf
Raffael Schmid <raffael@yux.ch>
2015-08-11 00:07:19 +0200

    temp_amqp with live data!

--- raspberry/temp_amqp.py
+++ raspberry/temp_amqp.py
@@ -1,53 +1,73 @@
 #!/usr/bin/env python
 
-import RPi.GPIO as GPIO
-import time
-import Adafruit_DHT
-import json
-import pika
+try:
+    import RPi.GPIO as GPIO
+    import time
+    import Adafruit_DHT
+    import spidev
+    import pika
+    import json
+    import os
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setwarnings(False)
+    GPIO.setmode(GPIO.BCM)
+    GPIO.setwarnings(True)
 
-uv_sensor = 14
-movement_sensor = 15
-temperature_sensor_pin = 18
-temperature_sensor_type = Adafruit_DHT.DHT22
+    LIGHT_SENSOR = 0 # analog
+    UV_SENSOR = 1 # analog
+    DUMMY_SENSOR = 2 # analog
+    MOVEMENT_SENSOR = 4
+    TEMPERATURE_SENSOR = 3
+    TEMPERATURE_SENSOR_TYPE = Adafruit_DHT.DHT22
 
-GPIO.setup(uv_sensor, GPIO.IN)
-GPIO.setup(movement_sensor, GPIO.IN)
-#GPIO.setup(temperature_sensor, GPIO.IN)
+    GPIO.setup(MOVEMENT_SENSOR, GPIO.IN)
 
-while True:
-    #uv_reading = GPIO.input(uv_sensor)
-    print "read movement..."
-    movement = GPIO.input(movement_sensor) == 1
-    print "read humidity and temperature..."
-    humidity, temperature = Adafruit_DHT.read_retry(temperature_sensor_type, temperature_sensor_pin)
+    spi = spidev.SpiDev()
+    spi.open(0,0)
 
-    #print "UV: %d" % uv_reading
-    print "MOVEMENT: %d" % movement
-    print "HUMIDITY: %f" % humidity
-    print "TEMPERATURE: %f" % temperature
+    # Function to read SPI data from MCP3008 chip
+    # Channel must be an integer 0-7
+    def ReadChannel(channel):
+        adc = spi.xfer2([1,(8+channel)<<4,0])
+        data = ((adc[1]&3) << 8) + adc[2]
+        return data
 
+    while True:
+        light_reading = ReadChannel(LIGHT_SENSOR)
+        uv_reading = ReadChannel(UV_SENSOR)
+        dummy_reading = ReadChannel(DUMMY_SENSOR)
 
-    print "publish..."
-    connection = pika.BlockingConnection(pika.ConnectionParameters('10.0.0.154'))
-    channel = connection.channel()
+        movement = GPIO.input(MOVEMENT_SENSOR) == 1
+        humidity, temperature = Adafruit_DHT.read_retry(TEMPERATURE_SENSOR_TYPE, TEMPERATURE_SENSOR)
 
-    message = { 'location': 'office', 'kind': 'movement', 'value': movement }
-    channel.basic_publish(exchange='measurements',
-            routing_key='office.movement', body=json.dumps(message))
+        print "======= reading ============"
+        print "LIGHT: %d" % light_reading
+        print "UV: %d" % uv_reading
+        print "DUMMY: %d" % dummy_reading
+        print "MOVEMENT: %d" % movement
+        print "HUMIDITY: %f" % humidity
+        print "TEMPERATURE: %f" % temperature
 
-    message = { 'location': 'office', 'kind': 'humidity', 'value': humidity }
-    channel.basic_publish(exchange='measurements',
-            routing_key='office.humidity', body=json.dumps(message))
+        print "======= publish ============"
+        credentials = pika.PlainCredentials('autohome', os.environ['AMQP_PASSWORD'])
+        parameters = pika.ConnectionParameters('10.0.0.12', 5672, '/', credentials)
+        connection = pika.BlockingConnection(parameters)
+        channel = connection.channel()
 
-    message = { 'location': 'office', 'kind': 'temperature', 'value': temperature }
-    channel.basic_publish(exchange='measurements',
-            routing_key='office.temperature', body=json.dumps(message))
-    connection.close()
+        messages = [
+                { 'location': 'office', 'kind': 'movement', 'state': movement },
 
-#    time.sleep(1)
+                { 'location': 'office', 'kind': 'light', 'value': light_reading },
+                { 'location': 'office', 'kind': 'uv', 'value': uv_reading },
+                { 'location': 'office', 'kind': 'dummy', 'value': dummy_reading },
+                { 'location': 'office', 'kind': 'humidity', 'value': humidity },
+                { 'location': 'office', 'kind': 'temperature', 'value': temperature },
+        ]
 
-GPIO.cleanup()
+        for message in messages:
+            channel.basic_publish(exchange='homeauto',
+                    routing_key='measurements.office', body=json.dumps(message))
+
+        time.sleep(1)
+
+finally:
+    GPIO.cleanup()
