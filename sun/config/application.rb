46ab02fa4180c2f09ea0af730292e8639b855c5a
Raffael Schmid <raffael@yux.ch>
2015-09-09 22:20:32 +0200

    add state processor

--- sun/config/application.rb
+++ sun/config/application.rb
@@ -22,6 +22,7 @@ module Sun
     end
     config.action_controller.action_on_unpermitted_parameters = :raise
     config.active_record.raise_in_transactional_callbacks = true
-    config.active_job.queue_adapter = :delayed_job
+
+    config.autoload_paths += %W(#{Rails.root}/app/workers)
   end
 end
